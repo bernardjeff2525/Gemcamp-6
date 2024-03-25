29e87c6b8a1ae3102cdb4fe9566c0349276a695b
Paula <ian.tz@topskyweb.com>
2024-03-25 13:44:16 +0800

    Rails Install

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+
+  def index;  end
+
+  def about; end
+
+  def contact_us; end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "home#index"
+
+  get '/home', to: 'home#index'
+  get '/about', to: 'home#about'
+  get '/contact-us', to: 'home#contact_us'
+end
