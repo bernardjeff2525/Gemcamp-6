878e679901971fd00e07327173cb6680cfa255b1
bernard <bernardjeff2525@gmail.com>
2024-04-02 12:02:04 +0800

    add: post create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :content))
+    if @post.save
+      redirect_to posts_path
+    end
+  end
 end
