1650540b9368f7b28d729c5c4d92660c5efa5411
bernard <bernardjeff2525@gmail.com>
2024-04-04 11:21:33 +0800

    add: destroy action to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,4 +36,11 @@ class PostsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = 'Post destroyed successfully'
+    redirect_to posts_path
+  end
 end
