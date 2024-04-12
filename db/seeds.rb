e9414eb1d6db5748db619fb8485000169af81981
bernard <bernardjeff2525@gmail.com>
2024-04-12 11:30:15 +0800

    add: post and category seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+categories = ['Technology', 'Travel', 'Lifestyle', 'Fashion', 'Food']
+categories.each do |category_name|
+  Category.create(name: category_name)
+end
+
+10.times do
+  post = Post.create(title: 'asdf', content: 'asdf')
+
+  rand(1..5).times do
+    category = Category.all.sample
+    next if post.categories.include? category
+
+    post.categories << category
+  end
+end
