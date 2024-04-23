e11874dd2de009dafac5282198a205833d177a15
bernard <bernardjeff2525@gmail.com>
2024-04-23 11:35:53 +0800

    add: genre enum to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+
+  enum genre: { client: 0, admin: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_22_025713) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_23_031049) do
   create_table "address_regions", charset: "utf8mb4", force: :cascade do |t|
     t.string "code"
     t.string "name"
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_22_025713) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
